3a1ab527b7a9df83f6f738d3c44c6a29568e1c6d
stevjoo <goodjestcom@gmail.com>
2025-11-20 11:18:44 +0700

    Commit No. 1: Building App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
--- app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_a/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.lab_week_11_a
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val preferenceWrapper = (application as SettingsApplication).settingsStore
+        val preferenceViewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SettingsViewModel(preferenceWrapper) as T
+            }
+        })[SettingsViewModel::class.java]
+
+        preferenceViewModel.textLiveData.observe(this) {
+            findViewById<TextView>(R.id.activity_main_text_view).text = it
+        }
+
+        findViewById<Button>(R.id.activity_main_button).setOnClickListener {
+            preferenceViewModel.saveText(
+                findViewById<EditText>(R.id.activity_main_edit_text).text.toString()
+            )
         }
     }
 }
